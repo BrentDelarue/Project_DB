388f79ad239d63c39852a74c01e38209ae95e974
BrentD <brent.delarue@student.howest.be>
2019-01-12 21:09:45 +0100

    k

--- Database/Database/Function1.cs
+++ Database/Database/Function1.cs
@@ -12,6 +12,8 @@ using System.Text;
 using System.Diagnostics;
 using System.Linq;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Reflection.Metadata;
 
 namespace Database
 {
@@ -42,29 +44,129 @@ namespace Database
             }
         }
     }
-    public static class PreRegistreren
+
+    public static class CheckUserName
     {
 
-        [FunctionName("PreRegistreren")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        [FunctionName("CheckUserName")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject user = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+                string query = $"SELECT * FROM c WHERE c.Naam = '{user["Naam"]}'";
+                var result = client.CreateDocumentQuery<User>(collectionUrl, query, queryOptions).AsEnumerable();
+                if (result.Count() > 0)
+                {
+                    return new OkObjectResult(true);
+                }
+                else
+                {
+                    return new OkObjectResult(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+
+    public static class CheckEmail
+    {
+
+        [FunctionName("CheckEmail")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject user = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+                string query = $"SELECT * FROM c WHERE c.Email = '{user["Email"]}'";
+                var result = client.CreateDocumentQuery<User>(collectionUrl, query, queryOptions).AsEnumerable();
+                if (result.Count() > 0)
+                {
+                    return new OkObjectResult(true);
+                }
+                else
+                {
+                    return new OkObjectResult(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+
+    public static class Login
+    {
+
+        [FunctionName("Login")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject user = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+                string query = $"SELECT * FROM c WHERE c.Naam = \"{user["Naam"]}\" and c.Wachtwoord = \"{user["Wachtwoord"]}\"";
+                var result = client.CreateDocumentQuery<User>(collectionUrl, query, queryOptions).AsEnumerable();
+                if (result.Count() == 0)
+                {
+                    return new OkObjectResult(false);
+                }
+                else
+                {
+                    return new OkObjectResult(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+
+    public static class UpdateProfile
+    {
+
+        [FunctionName("UpdateProfile")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = null)] HttpRequest req, ILogger log)
         {
             try
             {
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 User user = JsonConvert.DeserializeObject<User>(requestBody);
-                user.Id = Guid.NewGuid();
-
                 Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
                 string key = Environment.GetEnvironmentVariable("CosmosKey");
-
                 DocumentClient client = new DocumentClient(serviceEndPoint, key);
                 var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
                 FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
-
-                string query = $"SELECT Naam FROM verkoop WHERE Users.Naam = '{user.Naam}'";
-                var result = client.CreateDocumentQuery<User>(collectionUrl, query, queryOptions).AsEnumerable().First();
-                Debug.WriteLine(result);
-                return new OkObjectResult(result);
+                string query = $"SELECT * FROM c WHERE c.Naam = \"{user.Naam}\" or c.Wachtwoord = \"{user.Wachtwoord}\"";
+                Document doc = client.CreateDocumentQuery<Document>(collectionUrl, query, queryOptions).AsEnumerable().SingleOrDefault();
+                MyPoco poco = (dynamic)doc;
+                Document updated = await client.ReadDocumentAsync(doc.SelfLink, poco);
+                return new OkObjectResult("cv");
             }
             catch (Exception ex)
             {
---
+++ Database/Database/MyPoco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public class MyPoco
+    {
+        public string Naam { get; set; }
+        public string Wachtwoord { get; set; }
+    }
+}
