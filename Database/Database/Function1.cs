030f7553f71f010cc04cbb33b927648ecc182576
BrentD <brent.delarue@student.howest.be>
2019-01-12 19:13:26 +0100

    k

--- Database/Database/Function1.cs
+++ Database/Database/Function1.cs
@@ -10,10 +10,12 @@ using Newtonsoft.Json;
 using Microsoft.Azure.Documents.Client;
 using System.Text;
 using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Database
 {
-    public static class Function1
+    public static class Registreren
     {
         [FunctionName("Registreren")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
@@ -21,8 +23,8 @@ namespace Database
             try
             {
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                User drink = JsonConvert.DeserializeObject<User>(requestBody);
-                drink.Id = Guid.NewGuid();
+                User user = JsonConvert.DeserializeObject<User>(requestBody);
+                user.Id = Guid.NewGuid();
 
                 Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
                 string key = Environment.GetEnvironmentVariable("CosmosKey");
@@ -30,8 +32,39 @@ namespace Database
                 DocumentClient client = new DocumentClient(serviceEndPoint, key);
                 var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
 
-                await client.CreateDocumentAsync(collectionUrl, drink);
+                await client.CreateDocumentAsync(collectionUrl, user);
                 return new OkObjectResult("cv");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+    public static class PreRegistreren
+    {
+
+        [FunctionName("PreRegistreren")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                User user = JsonConvert.DeserializeObject<User>(requestBody);
+                user.Id = Guid.NewGuid();
+
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+
+                string query = $"SELECT Naam FROM verkoop WHERE Users.Naam = '{user.Naam}'";
+                var result = client.CreateDocumentQuery<User>(collectionUrl, query, queryOptions).AsEnumerable().First();
+                Debug.WriteLine(result);
+                return new OkObjectResult(result);
             }
             catch (Exception ex)
             {
