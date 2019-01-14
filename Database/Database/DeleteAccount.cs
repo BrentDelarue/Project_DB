88204a0b40e01ccf74bd2784c3bee46e00753a1b
BrentD <brent.delarue@student.howest.be>
2019-01-14 19:01:24 +0100

    getemail

---
+++ Database/Database/DeleteAccount.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Database
+{
+    public static class DeleteAccount
+    {
+        [FunctionName("DeleteAccount")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject user = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Users");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+                string query = $"SELECT * FROM c WHERE c.Naam = '{user["Naam"]}'";
+                var result = client.CreateDocumentQuery<JObject>(collectionUrl, query, queryOptions).AsEnumerable().FirstOrDefault();
+                Debug.WriteLine(result["user"].ToString());
+                var response = await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri("streetworkout", "Users", result["user"].ToString()));
+                return new OkObjectResult(response.StatusCode.ToString());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
