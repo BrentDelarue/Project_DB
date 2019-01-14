fc64ad060f875d02e7be4c78e5b5c77146701a48
BrentD <brent.delarue@student.howest.be>
2019-01-14 09:13:57 +0100

    sendgrid added

---
+++ Database/Database/MailService.cs
@@ -0,0 +1,75 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Database
+{
+    public static class MailService
+    {
+        [FunctionName("MailService")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject user = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Random rnd = new Random();
+                int length = rnd.Next(10, 15);
+                string wachtwoord = "";
+                for (int i = 0; i < length; i++)
+                {
+                    Random rnd2 = new Random();
+                    int number = rnd.Next(3, 6);
+                    if (number % 3 == 0)
+                    {
+                        Random rnd3 = new Random();
+                        string element = rnd.Next(0, 10).ToString();
+                        wachtwoord += element;
+                    }
+                    else if (number % 4 == 0)
+                    {
+                        var chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+                        Random rnd3 = new Random();
+                        int index = rnd.Next(0, chars.Length);
+                        wachtwoord += chars[index];
+                    }
+                    else if (number % 5 == 0)
+                    {
+                        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+                        Random rnd3 = new Random();
+                        int index = rnd.Next(0, chars.Length);
+                        wachtwoord += chars[index];
+                    }
+                }
+                var client = new SendGridClient(Environment.GetEnvironmentVariable("SendGrid_API"));
+                var from = new EmailAddress("nmctstreetworkout@outlook.com", "StreetWorkout");
+                var subject = "Aanvraag voorlopig wachtwoord";
+                var to = new EmailAddress(user["Email"].ToString(), user["Naam"].ToString());
+                var plainTextContent = $"Beste {user["Naam"].ToString()}\nU heeft een nieuw wachtwoord aangevraagd in de app StreetBeat.\nVolgend wachtwoord is uw nieuw voorlopig wachtwoord: {wachtwoord}\nWe raden u tensterkste aan om uw wachtwoord te veranderen na het gebruiken van dit wachtwoord.\n\nGroeten support StreetBeat.";
+                var htmlContent = $"Beste {user["Naam"].ToString()}{Environment.NewLine}{Environment.NewLine}U heeft een nieuw wachtwoord aangevraagd in de app StreetBeat.{Environment.NewLine}Volgend wachtwoord is uw nieuw voorlopig wachtwoord: {wachtwoord}{Environment.NewLine}We raden u tensterkste aan om uw wachtwoord te veranderen na het gebruiken van dit wachtwoord.{Environment.NewLine}{Environment.NewLine}Groeten support StreetBeat.";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+                Debug.WriteLine(htmlContent);
+                Debug.WriteLine("send");
+                return new OkObjectResult(200);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
