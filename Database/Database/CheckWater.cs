22363f7730c6ac33efe9700a170a818258fbab89
BrentD <brent.delarue@student.howest.be>
2019-01-19 20:12:29 +0100

    all operations water

---
+++ Database/Database/CheckWater.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Documents.Client;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Database
+{
+    public static class CheckWater
+    {
+        [FunctionName("CheckWater")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject userCheck = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Data");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+                string query = $"SELECT * FROM c WHERE c.Naam = \"{userCheck["Naam"]}\" and c.Datum = \"{userCheck["Datum"]}\" and c.Type = \"Water\"";
+                var result = client.CreateDocumentQuery<JObject>(collectionUrl, query, queryOptions).AsEnumerable();
+                if (result.Count() > 0)
+                {
+                    return new OkObjectResult(true);
+                }
+                else
+                {
+                    return new OkObjectResult(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Database/Database/DeleteWater.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using System.Diagnostics;
+using Microsoft.Azure.Documents;
+
+namespace Database
+{
+    public static class DeleteWater
+    {
+        [FunctionName("DeleteWater")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject user = JsonConvert.DeserializeObject<JObject>(requestBody);
+                Uri serviceEndPoint = new Uri(Environment.GetEnvironmentVariable("CosmosEndPoint"));
+                string key = Environment.GetEnvironmentVariable("CosmosKey");
+                DocumentClient client = new DocumentClient(serviceEndPoint, key);
+                var collectionUrl = UriFactory.CreateDocumentCollectionUri("streetworkout", "Data");
+                FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+                string query = $"SELECT * FROM c WHERE c.Naam = \"{user["Naam"]}\" and c.Type = \"Water\"";
+                var result = client.CreateDocumentQuery<JObject>(collectionUrl, query, queryOptions).AsEnumerable();
+                foreach (JObject oefening in result)
+                {
+                    await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri("streetworkout", "Data", oefening["id"].ToString()), new RequestOptions { PartitionKey = new PartitionKey("Water") });
+                }
+                return new OkObjectResult(200);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
