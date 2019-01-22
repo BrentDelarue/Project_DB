d33f42bc8311c0b0be621ce3a739051786a15e88
BrentD <brent.delarue@student.howest.be>
2019-01-22 15:55:32 +0100

    delete picture

---
+++ Database/Database/DeleteProfilePicture.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Diagnostics;
+
+namespace Database
+{
+    public static class DeleteProfilePicture
+    {
+        [FunctionName("DeleteProfilePicture")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject naam = JsonConvert.DeserializeObject<JObject>(requestBody);
+                CloudStorageAccount _cloudStorageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("BlobStorageAccount"));
+
+                CloudBlobClient blobClient = _cloudStorageAccount.CreateCloudBlobClient();
+
+                CloudBlobContainer container = blobClient.GetContainerReference("profilepicture");
+                
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(naam["Naam"] + ".jpg");
+                await blockBlob.DeleteIfExistsAsync();
+                return new StatusCodeResult(200);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Database/Database/GetProfilePicture.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+
+namespace Database
+{
+    public static class GetProfilePicture
+    {
+        [FunctionName("GetProfilePicture")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject naam = JsonConvert.DeserializeObject<JObject>(requestBody);
+                CloudStorageAccount _cloudStorageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("BlobStorageAccount"));
+                
+                CloudBlobClient blobClient = _cloudStorageAccount.CreateCloudBlobClient();
+                
+                CloudBlobContainer container = blobClient.GetContainerReference("profilepicture");
+                
+                SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+                sasConstraints.SharedAccessStartTime = DateTimeOffset.UtcNow.AddMinutes(-5);
+                sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(24);
+                sasConstraints.Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write;
+                
+                string sasBlobToken = container.GetSharedAccessSignature(sasConstraints);
+                
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(naam["Naam"] + ".jpg");
+
+                var url = new Uri(blockBlob.Uri + sasBlobToken);
+                return new OkObjectResult(url);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
