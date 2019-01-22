625dbdbf628106d59481881828a3c6334f20ee3a
BrentD <brent.delarue@student.howest.be>
2019-01-22 13:26:21 +0100

    postprofilepicture

---
+++ Database/Database/ProfilePicture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Database
+{
+    public class ProfilePicture
+    {
+        public string Naam { get; set; }
+        public Stream stream { get; set; }
+    }
+}
